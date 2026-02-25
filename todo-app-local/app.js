a9acc4989c411f8d8707b0d247e661d76b87c195
dkdk29 <20127211@student.hcmus.edu.vn>
2026-02-25 22:00:59 +0700

    Test CI/CD: Add full app.js code for pipeline verification

---
+++ todo-app-local/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+// Kết nối MongoDB local
+mongoose.connect('mongodb://localhost:27017/todo-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const TodoSchema = new mongoose.Schema({ task: String });
+const Todo = mongoose.model('Todo', TodoSchema);
+
+// Trang chủ
+app.get('/', (req, res) => {
+  res.send('Todo App v4 - Khoa DevOps Intern 2026 - Test CI/CD thành công!');
+});
+
+// Lấy tất cả todos
+app.get('/todos', async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+// Thêm todo mới
+app.post('/todos', async (req, res) => {
+  const todo = new Todo({ task: req.body.task });
+  await todo.save();
+  res.json(todo);
+});
+
+app.listen(port, () => {
+  console.log(`Todo App running on http://localhost:${port}`);
+});
